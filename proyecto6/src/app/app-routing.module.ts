f2e4ea17b6e07d8d9d86f17909dbe5612723b276
MarioRoblesInsa <roblesinsamario@gmail.com>
2021-02-26 15:48:25 +0100

    a

---
+++ proyecto6/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListarMascotasComponent } from './listar-mascotas/listar-mascotas.component';
+import { AgregarMascotaComponent } from './agregar-mascota/agregar-mascota.component';
+
+const routes: Routes = [  
+{ path: "mascotas", component: ListarMascotasComponent },
+{ path: "mascotas/agregar", component: AgregarMascotaComponent },
+{ path: "", redirectTo: "/mascotas", pathMatch: "full" },// Cuando es la raíz
+{ path: "**", redirectTo: "/mascotas" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ proyecto6/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListarMascotasComponent } from './listar-mascotas/listar-mascotas.component';
+import { AgregarMascotaComponent } from './agregar-mascota/agregar-mascota.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarMascotasComponent,
+    AgregarMascotaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
